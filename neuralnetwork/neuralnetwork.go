1507385159b631ba9374062107b28a4cfa7d71c5
Jonas Trevisan <jonhkr@gmail.com>
2014-07-08 23:09:57 -0300

    make the neural network marshalable

--- neuralnetwork/neuralnetwork.go
+++ neuralnetwork/neuralnetwork.go
@@ -36,123 +36,123 @@ func dsigmoid(y float64) float64 {
 
 type NeuralNetwork struct {
 	// Number of input, hidden and output nodes
-	ni, nh, no int
+	NI, NH, NO int
 	// Whether it is regression or not
-	regression bool
+	Regression bool
 	// Activations for nodes
-	ai, ah, ao []float64
+	AI, AH, AO []float64
 	// Weights
-	wi, wo [][]float64
+	WI, WO [][]float64
 	// Last change in weights for momentum
-	ci, co [][]float64
+	CI, CO [][]float64
 }
 
-func New(ni, nh, no int, regression bool) *NeuralNetwork {
-	nn := &NeuralNetwork{ni: ni + 1, nh: nh + 1, no: no, regression: regression}
+func New(NI, NH, NO int, Regression bool) *NeuralNetwork {
+	nn := &NeuralNetwork{NI: NI + 1, NH: NH + 1, NO: NO, Regression: Regression}
 
-	nn.ai = vector(nn.ni, 1.0)
-	nn.ah = vector(nn.nh, 1.0)
-	nn.ao = vector(nn.no, 1.0)
+	nn.AI = vector(nn.NI, 1.0)
+	nn.AH = vector(nn.NH, 1.0)
+	nn.AO = vector(nn.NO, 1.0)
 
-	nn.wi = matrix(nn.ni, nn.nh)
-	nn.wo = matrix(nn.nh, nn.no)
+	nn.WI = matrix(nn.NI, nn.NH)
+	nn.WO = matrix(nn.NH, nn.NO)
 
-	for i := 0; i < nn.ni; i++ {
-		for j := 0; j < nn.nh; j++ {
-			nn.wi[i][j] = random(-1, 1)
+	for i := 0; i < nn.NI; i++ {
+		for j := 0; j < nn.NH; j++ {
+			nn.WI[i][j] = random(-1, 1)
 		}
 	}
 
-	for i := 0; i < nn.nh; i++ {
-		for j := 0; j < nn.no; j++ {
-			nn.wo[i][j] = random(-1, 1)
+	for i := 0; i < nn.NH; i++ {
+		for j := 0; j < nn.NO; j++ {
+			nn.WO[i][j] = random(-1, 1)
 		}
 	}
 
-	nn.ci = matrix(nn.ni, nn.nh)
-	nn.co = matrix(nn.nh, nn.no)
+	nn.CI = matrix(nn.NI, nn.NH)
+	nn.CO = matrix(nn.NH, nn.NO)
 
 	return nn
 }
 
 func (nn *NeuralNetwork) Update(inputs []float64) []float64 {
-	if len(inputs) != nn.ni-1 {
+	if len(inputs) != nn.NI-1 {
 		fmt.Println("Error: wrong number of inputs")
 		return []float64{} // should return error
 	}
 
-	for i := 0; i < nn.ni-1; i++ {
-		nn.ai[i] = inputs[i]
+	for i := 0; i < nn.NI-1; i++ {
+		nn.AI[i] = inputs[i]
 	}
 
-	for i := 0; i < nn.nh-1; i++ {
+	for i := 0; i < nn.NH-1; i++ {
 		var sum float64 = 0.0
-		for j := 0; j < nn.ni; j++ {
-			sum += nn.ai[j] * nn.wi[j][i]
+		for j := 0; j < nn.NI; j++ {
+			sum += nn.AI[j] * nn.WI[j][i]
 		}
-		nn.ah[i] = sigmoid(sum)
+		nn.AH[i] = sigmoid(sum)
 	}
 
-	for i := 0; i < nn.no; i++ {
+	for i := 0; i < nn.NO; i++ {
 		var sum float64 = 0.0
-		for j := 0; j < nn.nh; j++ {
-			sum += nn.ah[j] * nn.wo[j][i]
+		for j := 0; j < nn.NH; j++ {
+			sum += nn.AH[j] * nn.WO[j][i]
 		}
-		if nn.regression {
-			nn.ao[i] = sum
+		if nn.Regression {
+			nn.AO[i] = sum
 		} else {
-			nn.ao[i] = sigmoid(sum)
+			nn.AO[i] = sigmoid(sum)
 		}
 	}
 
-	return nn.ao
+	return nn.AO
 }
 
 func (nn *NeuralNetwork) BackPropagate(targets []float64, lRate, mFactor float64) float64 {
-	if len(targets) != nn.no {
+	if len(targets) != nn.NO {
 		fmt.Println("Error: wrong number of target values")
 		return 0.0
 	}
 
-	output_deltas := vector(nn.no, 0.0)
-	for i := 0; i < nn.no; i++ {
-		output_deltas[i] = targets[i] - nn.ao[i]
+	output_deltas := vector(nn.NO, 0.0)
+	for i := 0; i < nn.NO; i++ {
+		output_deltas[i] = targets[i] - nn.AO[i]
 
-		if !nn.regression {
-			output_deltas[i] = dsigmoid(nn.ao[i]) * output_deltas[i]
+		if !nn.Regression {
+			output_deltas[i] = dsigmoid(nn.AO[i]) * output_deltas[i]
 		}
 	}
 
-	hidden_deltas := vector(nn.nh, 0.0)
-	for i := 0; i < nn.nh; i++ {
+	hidden_deltas := vector(nn.NH, 0.0)
+	for i := 0; i < nn.NH; i++ {
 		var e float64 = 0.0
 
-		for j := 0; j < nn.no; j++ {
-			e += output_deltas[j] * nn.wo[i][j]
+		for j := 0; j < nn.NO; j++ {
+			e += output_deltas[j] * nn.WO[i][j]
 		}
-		hidden_deltas[i] = dsigmoid(nn.ah[i]) * e
+		hidden_deltas[i] = dsigmoid(nn.AH[i]) * e
 	}
 
-	for i := 0; i < nn.nh; i++ {
-		for j := 0; j < nn.no; j++ {
-			change := output_deltas[j] * nn.ah[i]
-			nn.wo[i][j] = nn.wo[i][j] + lRate*change + mFactor*nn.co[i][j]
-			nn.co[i][j] = change
+	for i := 0; i < nn.NH; i++ {
+		for j := 0; j < nn.NO; j++ {
+			change := output_deltas[j] * nn.AH[i]
+			nn.WO[i][j] = nn.WO[i][j] + lRate*change + mFactor*nn.CO[i][j]
+			nn.CO[i][j] = change
 		}
 	}
 
-	for i := 0; i < nn.ni; i++ {
-		for j := 0; j < nn.nh; j++ {
-			change := hidden_deltas[j] * nn.ai[i]
-			nn.wi[i][j] = nn.wi[i][j] + lRate*change + mFactor*nn.ci[i][j]
-			nn.ci[i][j] = change
+	for i := 0; i < nn.NI; i++ {
+		for j := 0; j < nn.NH; j++ {
+			change := hidden_deltas[j] * nn.AI[i]
+			nn.WI[i][j] = nn.WI[i][j] + lRate*change + mFactor*nn.CI[i][j]
+			nn.CI[i][j] = change
 		}
 	}
 
 	var e float64 = 0.0
 
 	for i := 0; i < len(targets); i++ {
-		e += 0.5 * math.Pow(targets[i]-nn.ao[i], 2)
+		e += 0.5 * math.Pow(targets[i]-nn.AO[i], 2)
 	}
 
 	return e
@@ -171,6 +171,10 @@ func (nn *NeuralNetwork) Train(patterns [][][]float64, iterations int, lRate, mF
 		}
 
 		errors[i] = e
+
+		if i%1000 == 0 {
+			fmt.Println(i, e)
+		}
 	}
 
 	return errors
